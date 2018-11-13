c9e9f1a3236abb33208e1a83a5c1cbbcb42065e3
Ludovic Bourgoin <ludovic.bourgoin@gmail.com>
2018-11-13 19:59:56 +0100

    added jQuery for tabs WIP

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
 // This is where it all goes :)
+
+$(document).ready(function($) {
+
+  $('.tab-container').hide();
+  $('.tab-container:first').show();
+  $('#tab1').addClass('active');
+
+  $('.tab_content').click(function(event) {
+    $('.tab_content').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_container').hide();
+
+  });
+});
